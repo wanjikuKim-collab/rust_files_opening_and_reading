6a5e44b08813668f8a52c1c90c3ecb0707b5e782
wanjikuKim-collab <wanjikukimani24@gmail.com>
2024-02-21 18:00:55 +0300

    Reading files into a buffer to improve performance

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
-use std::fs::File;
 use std::path::Path;
-use std::io::Read;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
 fn main() {
     // opening a file
     let file_path = Path::new("src").join("hello.txt");
-    let mut txt_file = File::open(file_path).expect("Failed to open file");
+    let txt_file = File::open(file_path).expect("Failed to open file");
 
-    //reading the file in vector
-    let mut contents = Vec::new();
-    txt_file.read_to_end(&mut contents).expect("Failed to open file");
-    println!("{:?}",contents); 
+    //reading the file in a buffer
+    let reader = BufReader::new(txt_file);
+    for line in reader.lines(){
+        let line = line;
+        println!("{:?}", line);
+    }
 }
