8c2d26774ea39b1ef1c0de3481ae1143a73f3084
wanjikuKim-collab <wanjikukimani24@gmail.com>
2024-02-21 13:00:12 +0300

    Updated code to read the file and removed the open file logic

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,16 @@
-use std::fs::File;
+// use std::fs::File;
+use std::fs;
 
-use std::io::Read;
+// use std::io::Read;
 use std::path::Path;
 fn main() {
     // opening a file
     let file_path = Path::new("src").join("hello.txt");
-    let mut txt_file = File::open(file_path).expect("Failed to open file");
+    // let mut txt_file = File::open(file_path).expect("Failed to open file");
 
-    // match file{
-    //     Ok(_file) => {
-    //         println!("file successfully opened!");
-    //     }
-    //     Err(error) => {
-    //         println!("Failed to open file: {:?}", error);
-    //     }
-    // }
+    //reading the file
+    let content = fs::read_to_string(file_path).expect("Failed to read file content");
+    println!("{}", content); 
 
    
 }
