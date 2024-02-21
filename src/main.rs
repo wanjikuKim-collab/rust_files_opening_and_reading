418c4f81dcea38117057b3d5475a31be323fd9c1
wanjikuKim-collab <wanjikukimani24@gmail.com>
2024-02-21 12:46:44 +0300

    Updated error handling from match to expect for opening file

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,20 @@
 use std::fs::File;
+
+use std::io::Read;
 use std::path::Path;
 fn main() {
     // opening a file
     let file_path = Path::new("src").join("hello.txt");
-    let file = File::open(file_path);
+    let mut txt_file = File::open(file_path).expect("Failed to open file");
 
-    match file{
-        Ok(_file) => {
-            println!("file successfully opened!");
-        }
-        Err(error) => {
-            println!("Failed to open file: {:?}", error);
-        }
-    }
+    // match file{
+    //     Ok(_file) => {
+    //         println!("file successfully opened!");
+    //     }
+    //     Err(error) => {
+    //         println!("Failed to open file: {:?}", error);
+    //     }
+    // }
+
+   
 }
