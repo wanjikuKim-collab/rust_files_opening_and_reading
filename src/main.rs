827484e72c8bff3149c970b850c108ee726c67f5
wanjikuKim-collab <wanjikukimani24@gmail.com>
2024-02-22 16:24:49 +0300

    MP4 file reading in binary into a buffer

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
 use std::path::Path;
 use std::fs::File;
-use std::io::{BufRead, BufReader};
-fn main() {
-    // opening a file
-    let file_path = Path::new("src").join("hello.txt");
-    let txt_file = File::open(file_path).expect("Failed to open file");
+use std::io::{BufReader, Read};
+fn main(){
+    // opening a file in read mode
+    let file_path = Path::new("src").join("How to use FFMPEG.mp4");
+    let mut mp4_file = File::open(file_path).expect("Failed to open file");
 
-    //reading the file in a buffer
-    let reader = BufReader::new(txt_file);
-    for line in reader.lines(){
-        let line = line;
-        println!("{:?}", line);
-    }
+    //reading file into Bufreader 
+    let mut mp4_reader = BufReader::new(&mut mp4_file);
+    
+    let mut data = Vec::new();
+    mp4_reader.read_to_end(&mut data).expect("Faileed to read file");
+
+    println!("{:?}",data); 
 }
